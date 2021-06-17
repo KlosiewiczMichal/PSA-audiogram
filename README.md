e2a7fc4aba720c4822abb9a673ff59d71b34d660
KlosiewiczMichal <63036857+KlosiewiczMichal@users.noreply.github.com>
2021-06-17 12:44:10 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Niedopasowni
+
+## Parametry projektu
+
+Nazwa:  pai-lab-4  
+Opis:   Projekt bazowy repozytorium na czwarte spotkanie laboratoryjne z przedmiotu *Programowanie Aplikacji Internetowych*.  
+Wersja: v1.0.0  
+
+## Informacje o studencie wykonującym ćwiczenie laboratoryjne
+
+Imię i nazwisko studenta:   Michał Kłosiewicz
+Numer albumu:               69095  
+Grupa studencka:            WEL18ET1S1  
+Data realizacji zajęć:      15.06.2021 r. 
---
+++ templates/mainTemplate.php
@@ -0,0 +1,82 @@
+<?php if (!isset($portal)) die(); ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <title>Portal</title>
+</head>
+
+<body>
+
+  <div id="topDiv" class="scafoldingDiv">
+    <div id="headerMainDiv">
+      <a href="index.php">Główna</a> |
+      <a href="index.php?action=showSearchForm">Szukaj książki</a> |
+      <a href="index.php?action=showBasket">Twój koszyk</a> |
+      <a href="index.php?action=showRegistrationForm">Rejestracja</a>
+    </div>
+    <div id="headerUserInfoDiv">
+      <?php if ($portal->zalogowany) : ?>
+        <div>Jesteś zalogowany jako: <?= $portal->zalogowany->nazwa ?></div>
+        <div><a href="index.php?action=logout">Wylogowanie</a></div>
+      <?php else : ?>
+        <div>Nie jesteś zalogowany.</div>
+        <div><a href="index.php?action=showLoginForm">Logowanie</a></div>
+      <?php endif ?>
+    </div>
+  </div>
+
+  <div id="centerDiv" class="scafoldingDiv">
+    <div id="mainContentDiv">
+
+      <?php if ($komunikat) : ?>
+        <div class="komunikat"><?= $komunikat; ?></div>
+      <?php endif; ?>
+
+      <?php
+      switch ($action):
+        case 'showLoginForm':
+          include('templates/loginForm.php');
+          break;
+        case 'showRegistrationForm':
+          $portal->showRegistrationForm();
+          break;
+        case 'showSearchForm':
+          //Wyświetlenie formularza rejestracyjnego
+          $portal->showSearchForm();
+          break;
+        case 'searchBook':
+          $portal->showSearchForm();
+          $portal->showSearchResult();
+          break;
+        case 'showBookDetails':
+          $portal->showBookDetails();
+          break;
+        case 'showBasket':
+          $portal->showBasket();
+          break;
+        case 'checkout':
+          $portal->checkout();
+          break;
+        case 'showLoginForm': 
+          // Wyświetlenie formularza logowania
+          include('templates/loginForm.php');
+          break;
+          case 'showRegistrationForm': 
+            // Wyświetlenie formularza wyszukiwania
+            $portal->showRegistrationForm();
+            break;
+        case 'showMain':
+        default:
+          include 'templates/innerContentDiv.php';
+      endswitch;
+      ?>
+    </div>
+  </div>
+  <div id="footerDiv" class="scafoldingDiv">
+    <p>Stopka strony</p>
+  </div>
+</body>
+</html>
